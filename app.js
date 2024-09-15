1d17948ae0a3aca11450c8891252b63acb8106ec
islam heddi <islamheddi19@gmail.com>
2024-09-15 12:13:46 +0200

    task tracker : added the delete

--- app.js
+++ app.js
@@ -28,48 +28,70 @@ if(arg[0] == undefined) {
 
 switch(arg[0]){
     case "add":
-        if(arg[1] == undefined){
-            console.log("unable to add a new task due to undefined values")
-            return;
-        }
-        id = arr.length + 1;
-        const information = {
-            id,
-            description: arg[1],
-            inProgress: false,
-            complete: false,
-            createdAt: new Date(),
-            updateAt: "none"
-        }
-        try{
-            arr.push(information)
-            const arrToString = JSON.stringify(arr)
-            fs.writeFileSync("tasks.json",arrToString)
-        }catch(err){
-            console.log("Error : " + err)
-        }
+            if(arg[1] == undefined){
+                console.log("unable to add a new task due to undefined values")
+                return;
+            }
+            id = arr.length + 1;
+            const information = {
+                id,
+                description: arg[1],
+                inProgress: false,
+                complete: false,
+                createdAt: new Date(),
+                updateAt: "none"
+            }
+            try{
+                arr.push(information)
+                const arrToString = JSON.stringify(arr)
+                fs.writeFileSync("tasks.json",arrToString)
+                console.log("added successfully");
+            }catch(err){
+                console.log("Error : " + err)
+            }
         break;
     case "list":
-        console.log(arr)
+            console.log(arr)
         break;
     case "update":
-        const updateId = parseInt(arg[1]);
-        if(isNaN(updateId)) {
-            console.log("the id should be numeric")
-            break;
-        }
-        if(arg[2] == undefined){
-            console.log("Hey enter the task")
-            break;
-        }
-        const indextodo = arr.findIndex(Element => Element.id == updateId)
-        if(indextodo <= -1){
-            console.log("there no task have this id, check the id and then try")
-            break;
-        }
-        arr[indextodo].description = arg[2];
-        arr[indextodo].updateAt = new Date();
-        fs.writeFileSync("tasks.json",JSON.stringify(arr))
+            const updateId = parseInt(arg[1]);
+            if(isNaN(updateId)) {
+                console.log("the id should be numeric")
+                break;
+            }
+            if(arg[2] == undefined){
+                console.log("Hey enter the task")
+                break;
+            }
+            const indextodo = arr.findIndex(Element => Element.id == updateId)
+            if(indextodo <= -1){
+                console.log("there no task have this id, check the id and then try")
+                break;
+            }
+            arr[indextodo].description = arg[2];
+            arr[indextodo].updateAt = new Date();
+            fs.writeFileSync("tasks.json",JSON.stringify(arr))
+            console.log(`id: ${indextodo} updated successfully`)
+        break;
+        case 'delete':
+            if(arg[1] == undefined){
+                console.log("Nope enter the id, its very important step")
+                break;
+            }
+            const deleteId = parseInt(arg[1])
+            if(isNaN(deleteId)){
+                console.log("the id should be numeric");
+                break;
+            }
+            const indexTask = arr.findIndex(Element => Element.id == deleteId)
+            if(indexTask <= -1){
+                console.log("there no task have this id, check the id and then try")
+                break;
+            }
+
+            arr.splice(indexTask,1)
+            fs.writeFileSync("tasks.json",JSON.stringify(arr))
+            console.log(`id: ${indexTask} deleted successfully`)
         break;
     default: 
         console.log("the first argument should be a crud operation or find all")
