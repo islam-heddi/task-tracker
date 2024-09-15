42a5e7621e28ecb4e4ee4401c946ab8d25d85f73
islam heddi <islamheddi19@gmail.com>
2024-09-15 11:57:48 +0200

    task tracker : added the updates

--- app.js
+++ app.js
@@ -21,28 +21,57 @@ const getData = () => {
 
 getData();
 
-if(arg[0] !== "add") console.log("the first argument should be a crud operation or find all")
-if(arg[0] == "add"){
-    if(arg[1] == undefined){
-        console.log("unable to add a new task due to undefined values")
-        return;
-    }
-    id = arr.length + 1;
-    const information = {
-        id,
-        description: arg[1],
-        inProgress: false,
-        complete: false,
-        createdAt: new Date(),
-        updateAt: "none"
-    }
-    try{
-        arr.push(information)
-        const arrToString = JSON.stringify(arr)
-        fs.writeFileSync("tasks.json",arrToString)
-    }catch(err){
-        console.log("Error : " + err)
-    }
-
-
+if(arg[0] == undefined) {
+    console.log("you havent perform any action");
+    return;
 }
+
+switch(arg[0]){
+    case "add":
+        if(arg[1] == undefined){
+            console.log("unable to add a new task due to undefined values")
+            return;
+        }
+        id = arr.length + 1;
+        const information = {
+            id,
+            description: arg[1],
+            inProgress: false,
+            complete: false,
+            createdAt: new Date(),
+            updateAt: "none"
+        }
+        try{
+            arr.push(information)
+            const arrToString = JSON.stringify(arr)
+            fs.writeFileSync("tasks.json",arrToString)
+        }catch(err){
+            console.log("Error : " + err)
+        }
+        break;
+    case "list":
+        console.log(arr)
+        break;
+    case "update":
+        const updateId = parseInt(arg[1]);
+        if(isNaN(updateId)) {
+            console.log("the id should be numeric")
+            break;
+        }
+        if(arg[2] == undefined){
+            console.log("Hey enter the task")
+            break;
+        }
+        const indextodo = arr.findIndex(Element => Element.id == updateId)
+        if(indextodo <= -1){
+            console.log("there no task have this id, check the id and then try")
+            break;
+        }
+        arr[indextodo].description = arg[2];
+        arr[indextodo].updateAt = new Date();
+        fs.writeFileSync("tasks.json",JSON.stringify(arr))
+        break;
+    default: 
+        console.log("the first argument should be a crud operation or find all")
+}
+
